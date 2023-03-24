f6872e33bd35b9821cc5b6a23231d0ccc35cfcf7
Konstantin Yurov <kkrd@icloud.com>
2023-03-24 19:14:20 +0300

    Added comments to script

--- telegramMessage.sh
+++ telegramMessage.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # Set the API token and chat ID
-apiToken="6184791569:AAEaA55F8T8LmLUktfmfXDDft9FI0dE7vq4"
-chatId="1552740824"
+apiToken="6184791569:AAEaA55F8T8LmLUktfmfXDDft9FI0dE7vq4" # This is token for your bot
 
-# Set the message text
-#message="Ну а хули"
+# How to get chat ID from telegram. Send the message to telegram bot, then open the link
+# https://api.telegram.org/bot${apiToken}/getUpdates
+chatId="1552740824" # This is chat ID or ID user by telegram
+
+# Set the message text, by default type a text with quotas in terminal.
+# Example: bash telegramMessage.sh "Test text"
 message=$1
 
 # Use the curl command to send the message
-# curl -s -X POST https://api.telegram.org/bot${apiToken}/sendMessage -d chat_id=${chatId} -d text="$message"
+curl -s -X POST https://api.telegram.org/bot${apiToken}/sendMessage -d chat_id=${chatId} -d text="$message"
 
-cat $message > /tmp/exportToBot && curl --socks5-basic -s -X POST https://api.telegram.org/bot${apiToken}/sendDocument -F chat_id=${chatId} -F document=@/tmp/exportToBot
+# cat $message > /tmp/exportToBot && curl --socks5-basic -s -X POST https://api.telegram.org/bot${apiToken}/sendDocument -F chat_id=${chatId} -F document=@/tmp/exportToBot
 
 # Clean Up
 rm -f /tmp/exportToBot
 
-
-
-
+exit 0
